93989fbb1ccbd64524cf837cfc755723b2af5f18
kell <kellcrivelaro92@gmail.com>
2024-03-26 15:18:39 -0300

    fix: server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 import { env } from './env/index.js'
 import { appRoutes } from './http/routes.js'
 import { schedule } from './scheduler/index.js'
+import cors from '@fastify/cors';
 
 const port = env.PORT
 const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
@@ -11,6 +12,14 @@ schedule()
 export const fastify = Fastify({
 	logger: true,
 })
+
+fastify.register(cors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', '*'],
+  credentials: true,
+  maxAge:  86400,
+});
 
 fastify.register(appRoutes)
 
