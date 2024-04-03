48b66d37676fb77114c1f275d19b502e0390517a
kell <kellcrivelaro92@gmail.com>
2024-04-03 08:57:28 -0300

    add https agent

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,16 @@
 import { env } from '../env/index.js'
+import https from 'https'
 import axios from 'axios'
 
 const apiUrl = env.API_URL
 
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+ });
+
 export const api = axios.create({
   baseURL: apiUrl,
+  httpsAgent,
   headers: {
     'Accept-Encoding': 'gzip, deflate, br',
   }
